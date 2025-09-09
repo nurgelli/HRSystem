6753df42b4543efd19741aa52d5fc2458d06f207
nurgelli <nt.interpretor@gmail.com>
2025-09-09 11:15:04 +0500

    restarting new Hr system

--- src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
+++ src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.hrsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HrSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
